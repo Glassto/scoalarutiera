d52f97ed8a2548615f311a8c4a787f80a2c34991
Glassto <102603092+Glassto@users.noreply.github.com>
2024-01-24 16:47:25 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,31 +1,43 @@
 let pressedButton = document.getElementsByClassName('btn');
 let changeTextOnClick = document.getElementById('changeable-text')
 
-pressedButton[0].style = 'box-shadow: 0 0 0 2px #469b38';
+
+function chk(code) {
+  console.log(code)
+}
+pressedButton[0].style = `box-shadow: 0 0 0 2px #469b38`;
+
 function focusButtons(index) {
-  if(index === 0) {
-    pressedButton[1].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[2].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[3].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[index].style = 'box-shadow: 0 0 0 2px #469b38';
-    changeTextOnClick.innerHTML = "Găsești toată legislația și teoria structurată <span>pe 5 cursuri</span>, cel mai important fiind cursul de legislație rutieră - <span>explicat prin animații 3D.</span>"
-  } else if (index === 1) {
-    pressedButton[0].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[2].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[3].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[index].style = 'box-shadow: 0 0 0 2px #fe9e69';
-    changeTextOnClick.innerHTML = "Găsești toate indicatoarele și marcajele rutiere <span>explicate pe înțelesul tuturor.</span>"
-  } else if (index === 2) {
-    pressedButton[0].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[1].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[3].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[index].style = 'box-shadow: 0 0 0 2px #2ea0b9';
-    changeTextOnClick.innerHTML = "Găsești toate întrebările de la examenul oficial DRPCIV, verificate și <span>explicate Audio-Video prin animații 3D</span>  de către profesioniști."
-  } else if (index === 3) {
-    pressedButton[0].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[1].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[2].style = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
-    pressedButton[index].style = 'box-shadow: 0 0 0 2px #ed3b3b';
-    changeTextOnClick.innerHTML = "Îți vei testa cunoștințele <span>acumulate deja din primii trei pași de învățare</span> la final de chestionar le va afișa pe acelea la care ai greșit."
+  const boxShadowDefault = 'box-shadow: 0px 0px 4px -2px rgba(0, 0, 0, 0.4)';
+  for (let i = 0; i < pressedButton.length; i++) {
+    pressedButton[i].style = boxShadowDefault;
   }
+
+  pressedButton[index].style = `box-shadow: 0 0 0 2px ${getButtonColor(index)}`;
+  changeTextOnClick.innerHTML = getButtonText(index);
+  chk(clearInterval(advancer))
+  
+}
+
+function getButtonColor(index) {
+  const colors = ['#469b38', '#fe9e69', '#2ea0b9', '#ed3b3b'];
+  return colors[index];
+}
+function getButtonText(index) {
+  const texts = [
+    "Găsești toată legislația și teoria structurată <span>pe 5 cursuri</span>, cel mai important fiind cursul de legislație rutieră - <span>explicat prin animații 3D.</span>",
+    "Găsești toate indicatoarele și marcajele rutiere <span>explicate pe înțelesul tuturor.</span>",
+    "Găsești toate întrebările de la examenul oficial DRPCIV, verificate și <span>explicate Audio-Video prin animații 3D</span>  de către profesioniști.",
+    "Îți vei testa cunoștințele <span>acumulate deja din primii trei pași de învățare</span> la final de chestionar le va afișa pe acelea la care ai greșit."
+  ];
+  return texts[index];
+}
+
+let j = -1;
+setInterval(automaticAdvance, 8000)
+function automaticAdvance() {
+  if (j > focusButtons.length + 1) {
+    j = 1-1
+  } else {j = ++j}
+  focusButtons(j);
 }
